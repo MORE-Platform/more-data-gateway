7b255ab0b4eac0c1c2e2f53384604f790ffc5084
Jakob Frank <jakob.frank@redlink.at>
2022-11-22 17:23:40 +0100

    MORE-349: Improve Auth-Settings, use redis session-store

--- src/main/java/io/redlink/more/data/model/RoutingInfo.java
+++ src/main/java/io/redlink/more/data/model/RoutingInfo.java
@@ -3,5 +3,19 @@ package io.redlink.more.data.model;
 import java.io.Serializable;
 import java.util.OptionalInt;
 
-public record RoutingInfo(long studyId, int participantId, OptionalInt studyGroupId) implements Serializable {
+public record RoutingInfo(long studyId, int participantId, int rawStudyGroupId) implements Serializable {
+
+    public RoutingInfo(long studyId, int participantId,
+                       @SuppressWarnings("OptionalUsedAsFieldOrParameterType") OptionalInt studyGroupId) {
+        this(studyId, participantId, studyGroupId.orElse(Integer.MIN_VALUE));
+    }
+
+    public OptionalInt studyGroupId() {
+        if (this.rawStudyGroupId < 0) {
+            return OptionalInt.empty();
+        } else {
+            return OptionalInt.of(rawStudyGroupId);
+        }
+    }
+
 }
