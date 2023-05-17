615e27b74892d2dca057b087ae44a83e366a462d
Mikolaj Luzak <mikolaj.luzak@redlink.at>
2023-05-17 16:24:44 +0200

    MORE-960: security config added for request and token encryption added

--- src/main/java/io/redlink/more/data/service/ExternalService.java
+++ src/main/java/io/redlink/more/data/service/ExternalService.java
@@ -2,16 +2,21 @@ package io.redlink.more.data.service;
 
 import io.redlink.more.data.model.ApiRoutingInfo;
 import io.redlink.more.data.repository.StudyRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExternalService {
     private final StudyRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ExternalService(StudyRepository repository) {
+    public ExternalService(StudyRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
     public ApiRoutingInfo getRoutingInfo(String apiToken, String participantId) {
-        return repository.getApiRoutingInfo(apiToken, participantId);
+        return repository.getApiRoutingInfo(
+                passwordEncoder.encode(apiToken),
+                participantId);
     }
 }
